f43de946b74494f97c4b720e641ef60388f90dbe
chenmi21 <danielmhchen@gmail.com>
2015-06-07 00:16:43 -0400

    Add take photo/gallery buttons to user main

--- app/src/main/java/com/jebora/jebora/UserMain.java
+++ app/src/main/java/com/jebora/jebora/UserMain.java
@@ -1,6 +1,15 @@
 package com.jebora.jebora;
 
 import android.app.Activity;
+import android.content.ContentResolver;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Matrix;
+import android.media.Image;
+import android.net.Uri;
+import android.os.Environment;
+import android.provider.MediaStore;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.ActionBarActivity;
 import android.support.v7.app.ActionBar;
@@ -17,7 +26,15 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.support.v4.widget.DrawerLayout;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.OutputStream;
 
 
 public class UserMain extends ActionBarActivity
@@ -105,8 +122,10 @@ public class UserMain extends ActionBarActivity
      */
     public static class ContentFragment extends Fragment {
 
+        private static final int CAMERA_REQUEST = 1;
+        private static final int GALLERY_REQUEST = 2;
         private static final String ARG_SECTION_TITLE = "section_title";
-
+        private ImageView showImage;
         /**
          * 返回根据title参数创建的fragment
          */
@@ -118,17 +137,68 @@ public class UserMain extends ActionBarActivity
             return fragment;
         }
 
-        public ContentFragment() {
-        }
-
         @Override
         public View onCreateView(LayoutInflater inflater, ViewGroup container,
                                  Bundle savedInstanceState) {
-            View rootView = inflater.inflate(R.layout.fragment_main, container, false);
-            TextView textView = (TextView) rootView.findViewById(R.id.section_label);
-            textView.setText(getArguments().getString(ARG_SECTION_TITLE));
-            return rootView;
+            if(getArguments().getString(ARG_SECTION_TITLE).equals("Jebora")){
+                View rootView = inflater.inflate(R.layout.fragment_camera, container, false);
+                showImage = (ImageView) rootView.findViewById(R.id.selected_image);
+                ImageButton cameraButton = (ImageButton) rootView.findViewById(R.id.camera_button);
+                ImageButton galleryButton = (ImageButton) rootView.findViewById(R.id.gallery_button);
+
+                cameraButton.setOnClickListener(new View.OnClickListener(){
+                    @Override
+                    public void onClick(View v) {
+                        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                        startActivityForResult(intent, CAMERA_REQUEST);
+                    }
+                });
+
+                galleryButton.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        Intent i;
+                        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+                            i = new Intent(Intent.ACTION_OPEN_DOCUMENT);
+                            i.addCategory(Intent.CATEGORY_OPENABLE);
+                            i.setType("image/*");
+                            startActivityForResult(i, GALLERY_REQUEST);
+                        } else {
+                            i = new Intent(Intent.ACTION_PICK, android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                            startActivityForResult(i, GALLERY_REQUEST);
+                        }
+                    }
+                });
+                return rootView;
+            }
+            else{
+                View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+                TextView textView = (TextView) rootView.findViewById(R.id.section_label);
+                textView.setText(getArguments().getString(ARG_SECTION_TITLE));
+                return rootView;
+            }
+
+        }
+        @Override
+        public void onActivityResult(int requestCode, int resultCode, Intent data) {
+            if(resultCode != RESULT_OK) return;
+
+            if(requestCode == CAMERA_REQUEST){
+                Bundle extras = data.getExtras();
+                Bitmap photo = (Bitmap) extras.get("data");
+                showImage.setImageBitmap(photo);
+            }
+            else if(requestCode == GALLERY_REQUEST){
+                Uri origUri = data.getData();
+                Bitmap bitmap = null;
+                try{
+                    ContentResolver cr = getActivity().getApplicationContext().getContentResolver();
+                    bitmap= MediaStore.Images.Media.getBitmap(cr, origUri);
+                } catch (Exception e){
+                    e.printStackTrace();
+                }
+                showImage.setImageBitmap(bitmap);
+            }
         }
     }
-
 }
