9e6fd37bea426e7c2b19c93eb3b6ed464af59d23
mshzhb <mshzhb@gmail.com>
2015-07-18 00:05:19 -0400

    add work flow for preview product & action bar

--- app/src/main/java/com/jebora/jebora/ProductSelect.java
+++ app/src/main/java/com/jebora/jebora/ProductSelect.java
@@ -1,8 +1,8 @@
 package com.jebora.jebora;
 
-import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
 import android.support.v7.widget.DefaultItemAnimator;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
@@ -16,7 +16,7 @@ import com.jebora.jebora.adapters.RecyclerItemClickListener;
 import com.jebora.jebora.models.ProductManager;
 
 
-public class ProductSelect extends Activity {
+public class ProductSelect extends ActionBarActivity {
 
     public RecyclerView mRecyclerView;
     private ProductAdapter mAdapter;
