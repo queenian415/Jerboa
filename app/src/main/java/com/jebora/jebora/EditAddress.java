4c2e347cd149a1fff2273c150925311d4d989f43
azraeljack <4312182lyp@gmail.com>
2015-08-02 09:16:20 +0800

    Add EditAdress

--- app/src/main/java/com/jebora/jebora/EditAddress.java
+++ app/src/main/java/com/jebora/jebora/EditAddress.java
@@ -1,16 +1,29 @@
 package com.jebora.jebora;
 
+import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.view.Gravity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.Toast;
+
+import com.jebora.jebora.Utils.ShippingInfo;
 
 
 public class EditAddress extends ActionBarActivity {
 
-    private static final String TYPE = "TYPE";
+    private static final String INFO = "INFO";
+
+    private static final String NO_NAME = "请输入收件人姓名";
+    private static final String NO_ADDRESS = "请输入地址";
+    private static final String NO_CITY = "请输入城市";
+    private static final String NO_COUNTRY = "请输入国家";
+    private static final String NO_POSTALCODE = "请输入邮编";
+    private static final String SUCCESS = "成功添加地址";
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,6 +39,64 @@ public class EditAddress extends ActionBarActivity {
 
     public void onClickFinish(View v) {
 
+        String result = addAdress();
+
+
+        switch (result){
+            case(NO_NAME):{
+                DisplayToastMessage(NO_NAME);
+                return;
+            }
+            case(NO_COUNTRY):{
+                DisplayToastMessage(NO_COUNTRY);
+                return;
+            }
+            case(NO_POSTALCODE):{
+                DisplayToastMessage(NO_POSTALCODE);
+                return;
+            }
+            case(NO_CITY):{
+                DisplayToastMessage(NO_CITY);
+                return;
+            }
+            case(NO_ADDRESS):{
+                DisplayToastMessage(NO_ADDRESS);
+                return;
+            }
+            case(SUCCESS):{
+                DisplayToastMessage(SUCCESS);
+                startActivity(new Intent(EditAddress.this,ManageAddress.class));
+            }
+        }
+
+    }
+
+    public void DisplayToastMessage(String message){
+        Toast toast = Toast.makeText(getApplicationContext(),
+                message, Toast.LENGTH_LONG);
+        toast.setGravity(Gravity.CENTER, 0, 0);
+        toast.show();
+    }
+
+    private String addAdress(){
+        EditText name = (EditText)findViewById(R.id.Name);
+        EditText address = (EditText)findViewById(R.id.Address);
+        EditText city = (EditText)findViewById(R.id.City);
+        EditText country = (EditText)findViewById(R.id.Country);
+        EditText postalcode = (EditText)findViewById(R.id.PostalCode);
+        if(name.getText().toString().equals(""))
+            return NO_NAME;
+        if(address.getText().toString().equals(""))
+            return NO_ADDRESS;
+        if(city.getText().toString().equals(""))
+            return NO_CITY;
+        if(country.getText().toString().equals(""))
+            return NO_COUNTRY;
+        if(postalcode.getText().toString().equals(""))
+            return NO_POSTALCODE;
+        ShippingInfo si = new ShippingInfo(name.getText().toString(),address.getText().toString(),city.getText().toString(),country.getText().toString(),postalcode.getText().toString());
+        ServerCommunication.saveShippingInfo(si);
+        return SUCCESS;
     }
 
     @Override
