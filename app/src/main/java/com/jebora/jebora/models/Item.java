29829e63338f8ac2ee64795d9ed2ff3afada1cbf
mshzhb <mshzhb@gmail.com>
2015-08-09 12:16:09 -0400

    add checkout item

---
+++ app/src/main/java/com/jebora/jebora/models/Item.java
@@ -0,0 +1,14 @@
+package com.jebora.jebora.models;
+
+import android.app.Notification;
+import android.graphics.Bitmap;
+
+/**
+ * Created by Larry on 8/9/15.
+ */
+public class Item {
+    public String name;
+    public String color;
+    public Bitmap finalImage;
+    public Bitmap image;
+}
